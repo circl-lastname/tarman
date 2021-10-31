35c4b45346935b282fc7f4b1eb6d29c2b44b6af7
circl <circl.lastname@gmail.com>
2021-10-31 21:04:39 +0100

    Add script that makes a deb package

---
+++ mkdeb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script makes a deb package for Tarman
+
+mkdir -p temp/usr/local/bin
+mkdir temp/DEBIAN
+cp tarman temp/usr/local/bin
+cat << EOF > temp/DEBIAN/control
+Package: tarman
+Version: 0.9.1
+Section: admin
+Priority: optional
+Architecture: all
+Maintainer: circl <circl.lastname@gmail.com>
+Homepage: https://github.com/circl-lastname/tarman
+Description: A package manager for installing directories. 
+EOF
+dpkg-deb --build temp tarman.deb
+rm -r temp
